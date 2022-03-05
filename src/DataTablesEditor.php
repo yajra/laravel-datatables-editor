a27bdcd911b801a05647f2f71610fbf90dd5cc90
Arjay Angeles <aqangeles@gmail.com>
2022-03-05 11:14:53 +0800

    Fix doc block.
    (cherry picked from commit 677c984de63a71e27c4dac8adeff438e5a82fc1d)

--- src/DataTablesEditor.php
+++ src/DataTablesEditor.php
@@ -47,7 +47,7 @@ abstract class DataTablesEditor
     protected $customActions = [];
 
     /**
-     * @var \Illuminate\Database\Eloquent\Model
+     * @var null|string|\Illuminate\Database\Eloquent\Model
      */
     protected $model = null;
 
@@ -228,7 +228,7 @@ abstract class DataTablesEditor
     /**
      * Resolve model to used.
      *
-     * @return Model|\Illuminate\Database\Eloquent\SoftDeletes
+     * @return Model
      */
     protected function resolveModel()
     {
