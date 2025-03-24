ba8a054884807ecb986b62d5ff0f075fb6ee9c3e
Arjay Angeles <aqangeles@gmail.com>
2025-03-25 04:10:21 +0800

    fix: editor events TModel

--- src/DataTablesEditor.php
+++ src/DataTablesEditor.php
@@ -18,10 +18,16 @@ use Illuminate\Http\Request;
  */
 abstract class DataTablesEditor
 {
+    /** @use \Yajra\DataTables\Concerns\WithCreateAction<TModel> */
     use Concerns\WithCreateAction;
+
+    /** @use \Yajra\DataTables\Concerns\WithEditAction<TModel> */
     use Concerns\WithEditAction;
-    use Concerns\WithForceDeleteAction;
+
+    /** @use \Yajra\DataTables\Concerns\WithRemoveAction<TModel> */
     use Concerns\WithRemoveAction;
+
+    use Concerns\WithForceDeleteAction;
     use Concerns\WithRestoreAction;
     use Concerns\WithUploadAction;
     use ValidatesRequests;
